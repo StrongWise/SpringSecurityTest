a7754a4fafe93a6ab442d26e493acee2043cee45
StrongWise <ghoul300@naver.com>
2023-05-22 17:12:31 +0900

    test: AuthTokenFilter test

--- src/main/java/com/example/springsecuritytest/config/WebSecurityConfig.java
+++ src/main/java/com/example/springsecuritytest/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.springsecuritytest.config;
 
+import com.example.springsecuritytest.jwt.AuthTokenFilter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -10,12 +12,18 @@ import org.springframework.security.web.SecurityFilterChain;
 /**
  * description :
  */
+@Slf4j
 @Configuration
 @EnableGlobalMethodSecurity(
         prePostEnabled = true)
 public class WebSecurityConfig {
     @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        log.info("WebSecurityConfig > {}", http.toString());
         http.cors().and().csrf().disable()
                 .exceptionHandling().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
---
+++ src/main/java/com/example/springsecuritytest/jwt/AuthTokenFilter.java
@@ -0,0 +1,24 @@
+package com.example.springsecuritytest.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * description :
+ */
+@Slf4j
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        log.info("AuthTokenFilter > {}", filterChain.toString());
+        filterChain.doFilter(request, response);
+    }
+}
